f8cb85fc65a3c8d38b61c598ee518b2543c51f81
KrimssmirK <72426749+KrimssmirK@users.noreply.github.com>
2022-06-15 23:37:21 +0900

    edit arguments

--- time_in_out_form.py
+++ time_in_out_form.py
@@ -5,9 +5,8 @@ from labelinput import LabelInput
 class TimeInOutForm(ttk.Frame):
   
   
-  def __init__(self, parent, data_var, *args, **kwargs):
+  def __init__(self, parent, *args, **kwargs):
     super().__init__(parent, *args, **kwargs)
-    self.data_var = data_var
     
     self._vars = {
       'Staff': tk.StringVar(),
@@ -19,31 +18,49 @@ class TimeInOutForm(ttk.Frame):
     LabelInput(
       self, 
       'Staff', 
-      ttk.Combobox, 
-      {
-        'textvariable': self._vars['Staff'],
-        'values': ['Ishiah', 'Rj', 'Lhen', 'Fe'] #temporary, this has to change from local to dynamically retrieving from the database.
-      }
+      input_class=ttk.Combobox, 
+      var = self._vars['Staff'],
+      #temporary, this has to change from local to dynamically retrieving from the database.
+      input_args =  {'values': ['Ishiah', 'Rj', 'Lhen', 'Fe']}
     ).grid()
     
     LabelInput(
       self,
       'Time In',
-      ttk.Entry,
-      {
-        'textvariable': self._vars['Time In']
-      }
+      var = self._vars['Time In']
     ).grid()
     
     LabelInput(
       self,
       'Time Out',
-      ttk.Entry,
-      {
-        'textvariable': self._vars['Time Out']
-      }
+      var = self._vars['Time Out']
     ).grid()
     
+    buttons = tk.Frame(self)
+    buttons.grid(sticky=tk.W + tk.E, row=4)
+    self.addbutton = ttk.Button(
+      buttons, text='Add', command=None
+    )
+    self.addbutton.pack(side=tk.RIGHT)
     
-    
-    
+    self.resetbutton = ttk.Button(
+      buttons, text='Reset', command=self.reset
+    )
+    self.resetbutton.pack(side=tk.RIGHT)    
+  
+  def reset(self):
+    for var in self._vars.values():
+      if isinstance(var, tk.BooleanVar):
+        var.set(False)
+      else:
+        var.set('')
+  
+  def get(self):
+    data = dict()
+    for key, variable in self._vars.items():
+      try:
+        data[key] = variable.get()
+      except tk.TclError:
+        message = f'Error in field: {key}. Data was not saved!'
+        raise ValueError(message)
+    return data

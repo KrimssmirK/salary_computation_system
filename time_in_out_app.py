1f28b373a7b96e482786d689183e47f0ad0f559f
KrimssmirK <72426749+KrimssmirK@users.noreply.github.com>
2022-06-15 23:37:09 +0900

    added csv functions

--- time_in_out_app.py
+++ time_in_out_app.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
 from jsonvar import JSONVar
+
+from datetime import datetime
+from pathlib import Path
+import csv
+
+'''apps'''
 from time_in_out_form import TimeInOutForm
 
 class TimeInOutApp(ttk.Frame):
@@ -9,10 +15,11 @@ class TimeInOutApp(ttk.Frame):
     super().__init__(*args, **kwargs)
     
     '''data that holds the application'''
-    self.jsonvar = JSONVar(self)
+    # self.jsonvar = JSONVar(self)
     
     '''components'''
-    TimeInOutForm(self, self.jsonvar).grid()
+    self.time_in_out_form = TimeInOutForm(self)
+    self.time_in_out_form.grid()
     
     
     
@@ -23,3 +30,22 @@ class TimeInOutApp(ttk.Frame):
     
     
     ''''''
+  
+  def _on_save(self):
+    datestring = datetime.today().strftime('%Y-%m-%d')
+    filename = 'time_in_out_record_{}.csv'.format(datestring)
+    newfile = not Path(filename).exists()
+
+    try:
+      data = self.time_in_out_form.get()
+    except ValueError as e:
+      print(str(e))
+      return
+    
+    with open(filename, 'a', newline='') as fh:
+      csvwriter = csv.DictWriter(fh, fieldnames=data.keys())
+      if newfile:
+        csvwriter.writeheader()
+      csvwriter.writerow(data)
+
+    

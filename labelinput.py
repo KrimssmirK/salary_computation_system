10a04192627aa03dfb307fb5d54716770d4df1df
KrimssmirK <72426749+KrimssmirK@users.noreply.github.com>
2022-06-15 23:36:55 +0900

    improved labelinput

--- labelinput.py
+++ labelinput.py
@@ -1,14 +1,49 @@
 '''compound widget'''
+import tkinter as tk
 from tkinter import ttk
+
 
 class LabelInput(ttk.Frame):
   """A label and input combined together"""
   
-  def __init__(self, parent, label, inp_cls, inp_args, *args, **kwargs):
+  def __init__(
+    self, parent, label, var, input_class=ttk.Entry, 
+    input_args=None, label_args=None, *args, **kwargs
+  ):
     super().__init__(parent, *args, **kwargs) # basic initialization of widgets
+    input_args = input_args or {}
+    label_args = label_args or {}
+    self.variable = var
+    self.variable.label_widget = self
     
-    self.label = ttk.Label(self, text=label)
-    self.input = inp_cls(self, **inp_args)
+    if input_class in (ttk.Checkbutton, ttk.Button):
+      input_args['text'] = label
+    else:
+      self.label = ttk.Label(self, text=label, **label_args)
+      self.label.grid(row=0, column=0, sticky=(tk.W + tk.E))
     
-    self.label.pack()
-    self.input.pack()
+    if input_class in (
+      ttk.Checkbutton, ttk.Button, ttk.Radiobutton
+    ):
+      input_args['variable'] = self.variable
+    else:
+      input_args['textvariable'] = self.variable
+    
+    if input_class == ttk.Radiobutton:
+      self.input = tk.Frame(self)
+      for v in input_args.pop('values', []):
+        button = ttk.Radiobutton(
+          self.input, value=v, text=v, **input_args
+        )
+        button.pack(
+          side=tk.LEFT, ipadx=10, ipady=2, expand=True, fill='x'
+        )
+    else:
+      self.input = input_class(self, **input_args)
+    
+    self.input.grid(row=1, column=0, sticky=(tk.W + tk.E))
+    self.columnconfigure(0, weight=1)
+    
+  def grid(self, sticky=(tk.E + tk.W), **kwargs):
+    """Override grid to add default sticky values"""
+    super().grid(sticky=sticky, **kwargs)
